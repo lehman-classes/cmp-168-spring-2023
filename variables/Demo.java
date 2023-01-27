10dbb401b8ff24a1084674086d133acb9359d3f9
Hendrix Tavarez <htavarez@gmail.com>
2023-01-26 21:38:59 -0500

    variables

---
+++ variables/Demo.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class Demo {
+  
+
+  public static void main(String[] args) {
+    
+    // final int VALUE = 10;
+    // VALUE = 2;
+    // System.out.println(VALUE);
+
+    // int a = 20;
+    // int b = 3;
+    // float c = (float)a / (float)b;
+    // System.out.printf("%.8f", c);
+
+    // String message = "Hello, class";
+    // System.out.println(message.concat(" of Spring 2023"));
+    Random random = new Random();
+    int x = random.nextInt();
+    System.out.println(x);
+    // creating a variable
+    // int number;
+    // number = 1; // and assigned a value to it
+    // char letter = 'A';
+    // int nextNumber = 7;
+
+    // int newNumber = number + nextNumber;
+    // System.out.println("The number is " + number);
+
+    // number = -2147483648 - 5;
+    // System.out.println("The number is " + number);
+    // System.out.println("New number is " + newNumber);
+    //
+    // System.out.println("The letter is " + letter);
+  }
+}

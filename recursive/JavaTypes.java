5840f2a45cff512c33ae81928d6f03e77c48a16b
Hendrix Tavarez <htavarez@gmail.com>
2023-04-27 19:41:51 -0400

    recursion stuff

---
+++ recursive/JavaTypes.java
@@ -0,0 +1,12 @@
+public class JavaTypes {
+
+  public static void main(String[] args) {
+
+    // Primitive Types
+    int num = 1;
+
+    // Reference Types
+    String str = "Hello World!";
+
+  }
+}
